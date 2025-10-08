296b2d9bc1e07554fe3e450d994dd53f095b4371
Wildfire72 <m.vierow00@gmail.com>
2025-10-08 09:47:50 -0400

    Made a rough outline to archive speakers

--- clockOut.php
+++ clockOut.php
@@ -1,12 +1,16 @@
 <?php
 require_once 'database/dbShifts.php';
+//require_once 'database/dbPersons.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $shiftID = $_POST['shiftID'];
     $description = $_POST['description'];
+    //$test = retrieve_persons_by_name ("Bob SPCA");
+    //alert($test);
 
     if (!empty($shiftID) && !empty($description)) {
-        clockOutByShiftID($shiftID, $description);
+        clockOutByShiftID($shiftID, $description)
+        //archive_volunteer($volunteerID);
         echo "Success";
     } else {
         echo "Missing shiftID or description.";
