0812ba50cdf1f51c59f1843a0dbf016a78e11a5e
CalebLineberry <calebalineberry@gmail.com>
2025-10-23 19:26:09 -0400

    Created untested mailing functions with filler subject/body

--- sendEmail.php
+++ sendEmail.php
@@ -1,4 +1,11 @@
 <?php
+/* Implemented by Caleb Lineberry by following guides created by Dave Hollingworth and Guide Realm.
+Hollingworth's guide: "Send email with PHP | Create a Working Contact Form Using PHP" https://www.youtube.com/watch?v=fIYyemqKR58&t=206s
+Guide Realm's guide: "How To Set Up Gmail SMTP Server - Full Guide" https://www.youtube.com/watch?v=ZfEK3WP73eY
+
+Mail only works if the recipient's email is of "@gmail.com"
+
+All mailing functions created by Caleb Lineberry */
 
 require_once('vendor/autoload.php');
 use PHPMailer\PHPMailer\PHPMailer;
@@ -6,7 +13,7 @@ use PHPMailer\PHPMailer\SMTP;
 
 $mail = new PHPMailer(true);
 
-//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;  //Uncomment if errors with mailing
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
@@ -16,14 +23,45 @@ $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
 $mail->Username = 'wlccoffeetalks@gmail.com';
-$mail->Password = '--------'; //10 minute fix to hide the password from the internet, will fix later
+$mail->Password = 'lawp sirm rxck ooqz'; //10 minute fix to hide the password from the internet, will fix later
+
 
 $mail->setFrom('wlccoffeetalks@gmail.com','WLC Coffee Talks');
-$mail->addAddress('calebalineberry@gmail.com','LebLeb');
 
-$mail->Subject='Hello World!';
-$mail->Body = 'Tuff';
 
-$mail->send();
+function sendEmail($to, $name, $subject, $message) {
+    global $mail;
+    $mail->addAddress($to, $name);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->send();
+}
+
+function sendFormConfirmation($to, $name) {
+    global $mail;
+    $mail->addAddress($to, $name);
+    $mail->Subject = "Interest Form Recieved";
+    $mail->Body = "Thank you for your interest in speaking for the Women's Leadership Colloquium's Coffee Talks! 
+    We will let you know when your form has been reviewed and accepted.";
+    $mail->send();
+}
+
+function sendFormApproved($to, $name) {
+    global $mail;
+    $mail->addAddress($to, $name);
+    $mail->Subject = "Interest Form Approved";
+    $mail->Body = "Your interest form for the Women's Leadership Coloquium Coffee Talks has been approved!
+    You will be notified when you have been scheduled for a Coffee Talk.";
+    $mail->send();
+}
+
+function sendScheduledSpeaker($to, $name, $date) {
+    global $mail;
+    $mail->addAddress($to, $name);
+    $mail->Subject = "Scheduled for Coffee Talk on **Date**";
+    $mail->Body = "You have been scheduled to speak on **Date**. 
+    Please email **real email** if you need to rechedule or cancel.";
+    $mail->send();
+}
 
 ?>
