9bebc527841f33d06eb19f0db4ca1653099a04de
Wildfire72 <m.vierow00@gmail.com>
2025-10-29 09:20:37 -0400

    Updated login page

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <title>Frederickburg SPCA Volunteer Management | Dashboard</title>
+    <title>Women's Leadership Colloquium | Dashboard</title>
     <style>
         * {
             box-sizing: border-box;
