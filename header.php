7afcd4c4773e182e2a077227808bdfaae1e84723
Wildfire72 <m.vierow00@gmail.com>
2025-11-03 00:23:55 -0500

    Started applying css changes to all pages

--- header.php
+++ header.php
@@ -836,7 +836,7 @@ if (date("H:i:s") > "18:19:59") {
         <div class="right-section">
 <a href="calendar.php">
 <div class="icon-butt">
-        <svg width="30" height="30" viewBox="0 0 24 24" fill="#294877" xmlns="http://www.w3.org/2000/svg">
+        <svg width="30" height="30" viewBox="0 0 24 24" fill="#800000" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 4C3 3.44772 3.44772 3 4 3H6V2C6 1.44772 6.44772 1 7 1C7.55228 1 8 1.44772 8 2V3H16V2C16 1.44772 16.4477 1 17 1C17.5523 1 18 1.44772 18 2V3H20C20.5523 3 21 3.44772 21 4V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V4ZM5 5V20H19V5H5ZM7 10H9V12H7V10ZM11 10H13V12H11V10ZM15 10H17V12H15V10ZM7 14H9V16H7V14ZM11 14H13V16H11V14ZM15 14H17V16H15V14Z"/>
         </svg>
 </div>
@@ -864,11 +864,13 @@ if (date("H:i:s") > "18:19:59") {
         <!-- Left Section: Logo & Nav Links -->
         <div class="left-section">
             <div class="logo-container">
-                <a href="index.php"><img src="images/actual_log.png" alt="Logo"></a>
+                <a href="index.php"><img src="images/real-logo1.png" alt="Logo"></a>
             </div>
+            <!--
                 <a href="viewCheckInOut.php" style="color: white; text-decoration: none;"><div class="date-box">Check In/Out</div></a>
+                -->
             <div class="nav-links">
-                <div class="nav-item">Volunteers
+                <div class="nav-item">Speakers
                     <div class="dropdown">
 
 <a href="VolunteerRegister.php" style="text-decoration: none;">
