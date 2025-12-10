c3ab559d1bfe382355016fa6eb60b20d3130c8ba
ren <zakbutt18@gmail.com>
2025-12-10 00:15:39 -0500

    Improvement/Database Credentials for Localhost and Siteground
    - Now works with sendReminders.php

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -1,13 +1,13 @@
 <?php
 date_default_timezone_set('America/New_York');
 /*
- * Copyright 2015 by Allen Tucker. 
- * This program is part of RMHP-Homebase, which is free software.  It comes with 
- * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * Copyright 2015 by Allen Tucker.
+ * This program is part of RMHP-Homebase, which is free software.  It comes with
+ * absolutely no warranty. You can redistribute and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation
  * (see <http://www.gnu.org/licenses/ for more information).
- * 
- * 
+ *
+ *
  */
 
 /*
@@ -19,21 +19,22 @@ date_default_timezone_set('America/New_York');
 date_default_timezone_set('America/New_York');
 
 function connect() {
-    $host = "localhost"; 
+    $host     = "localhost";
     $database = "wlcms_db";
-    $user = "wlcms_db";
-    $pass = "wlcms_db";
-    if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'jenniferp160.sg-host.com') {
-        $user = 'uknrzrk8sj1e7';
-        $database = 'dbkzrh4cfmxbt0';
-        $pass = 'fxextih7mssg';
-    } 
+    $user     = "wlcms_db";
+    $pass     = "wlcms_db";
+    if (   isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'jenniferp213.sg-host.com'    # For entire website
+        || isset($_SERVER['SITE_NAME'])   && $_SERVER['SITE_NAME']   == 'jenniferp213.sg-host.com') { # For sendReminders.php
+        $database = '--------------------';
+        $user     = '--------------------';
+        $pass     = '--------------------';
+    }
     $con = mysqli_connect($host,$user,$pass,$database);
     if (!$con) { echo "not connected to server"; return mysqli_error($con);}
     $selected = mysqli_select_db($con,$database);
     if (!$selected) { echo "database not selected"; return mysqli_error($con); }
     else return $con;
-    
+
 }
 
 ?>
